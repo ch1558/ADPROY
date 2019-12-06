ef2cd32140ee6588b70670ccf49c20bdf43674f9
Karen Beltran <brigid2427@gmail.com>
2019-12-06 15:21:48 -0500

    Update modal.js

--- public/js/modal.js
+++ public/js/modal.js
@@ -12,3 +12,13 @@ function agregarModal(titulo_anteproyecto, resumen_anteproyecto, codigo_modalida
 function agregarCodigo(codigo_anteproyecto){
     document.getElementById("codigo").value = codigo_anteproyecto;
 }
+
+function verificarAnteproyecto(titulo_anteproyecto, resumen_anteproyecto, codigo_modalidad, codigo_grupo, codigo_tema, codigo_anteproyecto, codigo_persona){
+    document.getElementById("codigo").innerHTML = codigo_anteproyecto;
+    document.getElementById("titulo").innerHTML = titulo_anteproyecto;
+    document.getElementById("resumen").innerHTML = resumen_anteproyecto;
+    document.getElementById("modalidad").innerHTML = codigo_modalidad;
+    document.getElementById("grupo").innerHTML = codigo_grupo;
+    document.getElementById("tema").innerHTML = codigo_tema;
+    document.getElementById("director").innerHTML = codigo_persona;
+}
